23aa3e84992f4c8728c44204b50fb1041a3da6e6
Stephen Benner <sbenner9@gmail.com>
2015-01-19 07:50:42 -0800

    update

--- 20-questions/game-intermediate.rb
+++ 20-questions/game-intermediate.rb
@@ -9,131 +9,132 @@
 
 # classes
 class Knowledge_tree
+
 end
 
 class EmptyKT < Knowledge_tree
-	def inspect
-		print "#{EmptyKT.new}"
-	end
+  def inspect
+    print "#{EmptyKT.new}"
+  end
 
-	# @return [NodeAnswer] A new answer node
-	def play
-		puts "Actually, I don't know jack. Let's fire up the learn-o-nator 5000! What were you in fact thinking of?"
-		new_thing = gets.chomp
-		puts "Holy $#!&*, looks like I learned something new today!!!\nI now know what '#{new_thing}' is!"
-		NodeAnswer.new new_thing
-	end
+  # @return [NodeAnswer] A new answer node
+  def play
+    puts "Actually, I don't know jack. Let's fire up the learn-o-nator 5000! What were you in fact thinking of?"
+    new_thing = gets.chomp
+    puts "Holy $#!&*, looks like I learned something new today!!!\nI now know what '#{new_thing}' is!"
+    NodeAnswer.new new_thing
+  end
 
-	def traverse_tree(addition = '')
-		yield addition + '[Empty node]' unless !$print_empty
-	end
+  def traverse_tree(addition = '')
+    yield addition + '[Empty node]' unless !$print_empty
+  end
 
-	def height
-		0
-	end
+  def height
+    0
+  end
 
-	def num_nodes
-		0
-	end
+  def num_nodes
+    0
+  end
 end
 
 class NodeAnswer < Knowledge_tree
-	attr_accessor :value
+  attr_accessor :value
 
-	def initialize(value)
-		self.value = value
-	end
+  def initialize(value)
+    self.value = value
+  end
 
-	def inspect
-		puts "#{NodeAnswer.new(self.value)}"
-	end
+  def inspect
+    puts "#{NodeAnswer.new(self.value)}"
+  end
 
-	# Recursively plays a game using given tree, returning modified tree as a result
-	# @return [Knowledge_tree] A game tree with state reflecting the actions of a single play-through
-	def play
-		puts "Are you thinking of '#{self.value}' by any chance? (Y/N)"
-		if gets.chomp =~ /Y|y/
-			puts 'Awwwwwwwwwwwwwwwwww yea, givin Stephen Hawking a run for his money now!'
-			self
-		else
-			puts 'IMPOSSIBRU!!!!!!'
-			learn_new_thang
-			self
-		end
-	end
+  # Recursively plays a game using given tree, returning modified tree as a result
+  # @return [Knowledge_tree] A game tree with state reflecting the actions of a single play-through
+  def play
+    puts "Are you thinking of '#{self.value}' by any chance? (Y/N)"
+    if gets.chomp =~ /Y|y/
+      puts 'Awwwwwwwwwwwwwwwwww yea, givin Stephen Hawking a run for his money now!'
+      self
+    else
+      puts 'IMPOSSIBRU!!!!!!'
+      learn_new_thang
+      self
+    end
+  end
 
-	# Initializes global hash with values for a new node, so it may be created as the root of a new tree
-	def learn_new_thang
-		puts 'I give up... You be one sneaky beezy. What were you thinking of?'
-		new_answer = gets.chomp
-		puts "Help me out here... Ask me a question I can use to discern between '#{new_answer}' and everything else."
-		new_question = gets.chomp
-		puts "If I ask you '#{new_question}', and you're thinking of '#{new_answer}', what is you're answer? (Y/N)"
-		distinction = gets.chomp
-		puts "Holy $#!&*, looks like I learned something new today!!!\nI now know what '#{new_answer}' is!"
-		$new_node = {:thing => new_answer, :question => new_question, :child => distinction}
-	end
+  # Initializes global hash with values for a new node, so it may be created as the root of a new tree
+  def learn_new_thang
+    puts 'I give up... You be one sneaky beezy. What were you thinking of?'
+    new_answer = gets.chomp
+    puts "Help me out here... Ask me a question I can use to discern between '#{new_answer}' and everything else."
+    new_question = gets.chomp
+    puts "If I ask you '#{new_question}', and you're thinking of '#{new_answer}', what is you're answer? (Y/N)"
+    distinction = gets.chomp
+    puts "Holy $#!&*, looks like I learned something new today!!!\nI now know what '#{new_answer}' is!"
+    $new_node = {:thing => new_answer, :question => new_question, :child => distinction}
+  end
 
-	def traverse_tree(addition = '')
-		yield addition + "A: #{self.value}"
-	end
+  def traverse_tree(addition = '')
+    yield addition + "A: #{self.value}"
+  end
 
-	def height
-		1
-	end
+  def height
+    1
+  end
 
-	def num_nodes
-		1
-	end
+  def num_nodes
+    1
+  end
 end
 
 class NodeQuestion < Knowledge_tree
-	attr_accessor :value, :yes, :no
+  attr_accessor :value, :yes, :no
 
-	def initialize(value, yes = EmptyKT.new, no = EmptyKT.new)
-		self.value = value
-		self.yes = yes
-		self.no = no
-	end
+  def initialize(value, yes = EmptyKT.new, no = EmptyKT.new)
+    self.value = value
+    self.yes = yes
+    self.no = no
+  end
 
-	def inspect
-		print "#{NodeQuestion.new(self.value)}" + "\t\t" + "#{self.yes.inspect}" + "\n\t\t" + "#{self.no.inspect}"
-	end
+  def inspect
+    print "#{NodeQuestion.new(self.value)}" + "\t\t" + "#{self.yes.inspect}" + "\n\t\t" + "#{self.no.inspect}"
+  end
 
-	# Recursively plays a game using given tree, returning modified tree as a result
-	# @return [Knowledge_tree] A game tree with state reflecting the actions of a single play-through
-	def play
-		puts self.value
-		gets.chomp =~ /Y|y/ ?
-				NodeQuestion.new(value, self.yes.play, self.no) :
-				NodeQuestion.new(value, self.yes, self.no.play)
-	end
+  # Recursively plays a game using given tree, returning modified tree as a result
+  # @return [Knowledge_tree] A game tree with state reflecting the actions of a single play-through
+  def play
+    puts self.value
+    gets.chomp =~ /Y|y/ ?
+        NodeQuestion.new(value, self.yes.play, self.no) :
+        NodeQuestion.new(value, self.yes, self.no.play)
+  end
 
-	# this traversal function recursively calls itself down throughout the tree, using 'yield' to evaluate the given
-	# procedure object, (a Ruby code block). When called with an argument, 'yield' passes it into the code block to be
-	# used as a parameter, allowing for arbitrary specification within each implementation of the method. This allows
-	# for some incredibly flexible coding tricks. In this case, each class calls 'yield' with a value specific to itself,
-	# which is the node value or empty. However, we can also prepend to this an argument received from the traversal
-	# method, meaning what gets evaluated at each node can be specified arbitrarily from previous callers in
-	# the recursion process, so that the method can exhibit different behavior depending on where in the tree hierarchy
-	# it gets evaluated. Thus, beautiful output in an outstandingly elegant and powerful fashion.
-	def traverse_tree(addition = '', &proc)
-		yield addition + "Q: #{self.value}"
-		depth = $total_height - self.height
-		# add 1 to depth because the root node has a depth of 0
-		self.yes.traverse_tree("\n"+"\t"*(depth+1), &proc)
-		self.no.traverse_tree("\n"+"\t"*(depth+1), &proc)
-	end
+  # this traversal function recursively calls itself down throughout the tree, using 'yield' to evaluate the given
+  # procedure object, (a Ruby code block). When called with an argument, 'yield' passes it into the code block to be
+  # used as a parameter, allowing for arbitrary specification within each implementation of the method. This allows
+  # for some incredibly flexible coding tricks. In this case, each class calls 'yield' with a value specific to itself,
+  # which is the node value or empty. However, we can also prepend to this an argument received from the traversal
+  # method, meaning what gets evaluated at each node can be specified arbitrarily from previous callers in
+  # the recursion process, so that the method can exhibit different behavior depending on where in the tree hierarchy
+  # it gets evaluated. Thus, beautiful output in an outstandingly elegant and powerful fashion.
+  def traverse_tree(addition = '', &proc)
+    yield addition + "Q: #{self.value}"
+    depth = $total_height - self.height
+    # add 1 to depth because the root node has a depth of 0
+    self.yes.traverse_tree("\n"+"\t"*(depth+1), &proc)
+    self.no.traverse_tree("\n"+"\t"*(depth+1), &proc)
+  end
 
-	def height
-		1 + [self.yes.height, self.no.height].max
-	end
+  def height
+    1 + [self.yes.height, self.no.height].max
+  end
 
-	def num_nodes
-		1 + self.yes.num_nodes + self.no.num_nodes
-	end
+  def num_nodes
+    1 + self.yes.num_nodes + self.no.num_nodes
+  end
 
-	alias :num_questions :num_nodes
+  alias :num_questions :num_nodes
 end
 
 # functions
@@ -141,25 +142,25 @@ end
 # oh this is sexy.
 # TODO: rewrite using 'Dir.glob'?
 def fname_incr(fname)
-	!File.exists?(fname) ?
-			fname :
-			/([0-9]+)\./.match(fname) ?
-					fname_incr(fname.sub(/([0-9]+)/) { |m| (m.to_i+1).to_s }) :
-					fname_incr(fname.sub(/[^\.]*/) { |m| m+'1' })
+  !File.exists?(fname) ?
+      fname :
+      /([0-9]+)\./.match(fname) ?
+          fname_incr(fname.sub(/([0-9]+)/) { |m| (m.to_i+1).to_s }) :
+          fname_incr(fname.sub(/[^\.]*/) { |m| m+'1' })
 end
 
 # this function however, is, dare I say... EVEN SEXIER?!?
 def parse_tree(lines)
-	line = lines.shift.strip! # take the first line from the array and remove whitespace
-	# note the use of || to default to using 'Q: ' as our base case here if the return value from 'slice!' is nil
-	case line.slice!(0..2) || 'Q: '
-		when 'Q: '
-			NodeQuestion.new(line, parse_tree(lines), parse_tree(lines))
-		when 'A: '
-			NodeAnswer.new(line)
-		else
-			EmptyKT.new
-	end
+  line = lines.shift.strip! # take the first line from the array and remove whitespace
+  # note the use of || to default to using 'Q: ' as our base case here if the return value from 'slice!' is nil
+  case line.slice!(0..2) || 'Q: '
+    when 'Q: '
+      NodeQuestion.new(line, parse_tree(lines), parse_tree(lines))
+    when 'A: '
+      NodeAnswer.new(line)
+    else
+      EmptyKT.new
+  end
 end
 
 # data
@@ -181,69 +182,69 @@ $print_empty = true
 $print_with_newline = false
 opt = 1
 while (1..5) === opt
-	begin
-		puts "\nWelcome to Stephen's guessing game!\n"
-		puts '1 - Play the game'
-		puts '2 - View current game tree'
-		puts '3 - Save game to text file'
-		puts '4 - Load game from text file'
-		puts "5 - Quit\n"
-		opt = gets.chomp.to_i
-		# read in user input
-		# ran into the issues of determining how deep a node was in the tree, for printing structured output correctly,
-		# so for now I thought it best to solve the problem using a somewhat 'hacky' but effective approach; globals!
-		# gross! haha. But it works and makes sense. The game is only processing one tree at a time, so it just maintains
-		# the current 'total' tree height and the traversal method calculates the node depth based on the difference.
-		# Obviously this breaks the flavor of our recursive program, but makes sense in as an exception since the information
-		# is not representable per tree-instance. I thought about using a class variable but what's the point? =)
-		$total_height = tree.height
-		case opt
-			when 1 then
-				puts "Think of something and I'll try to guess it."
-				puts "When you're ready, enter 'Y' or 'N'"
-				tree = tree.play if gets.chomp =~ /Y|y/
-				# this it the only part of my program that feels 'uncomfortable'. It breaks with the elegance the rest of the
-				# control flow exhibits. We have to implement this out of recursion however, if we are going to modify the tree
-				# at the root level. It's a bit 'hacky', but does the job.
-				if $new_node
-					tree = $new_node[:child] =~ /Y|y/ ?
-							NodeQuestion.new($new_node[:question], NodeAnswer.new($new_node[:thing]), tree) :
-							NodeQuestion.new($new_node[:question], tree, NodeQuestion.new($new_node[:thing]))
-				end
-			when 2 then
-				puts ''
-				tree.traverse_tree { |val| print val }
-				puts ''
-			when 3 then
-				puts 'Enter a filename, including extension:'
-				fname = gets.chomp
-				if File.exists? fname
-					puts 'File already exists - overwrite?'
-					file = (gets.chomp =~ /Y|y/) ?
-							File.open(fname, 'w+') :
-							File.open(fname_incr(fname), 'w+')
-				else
-					file = File.open(fname, 'w+')
-				end
-				tree.traverse_tree { |val| file.print !$print_with_newline ? val : val + "\n" }
-				file.close
-				puts "Game data saved to '#{file.path}'"
-			when 4 then
-				puts 'Enter a filename, with or without extension:'
-				fname = 'game.txt'
-				files = Dir.glob(fname)
-				if files.empty?
-					puts 'File does not exist.'
-				else
-					file = File.open(files.first)
-					file.gets.strip[0..2] == 'Q: ' ?
-							tree = parse_tree(file.readlines) :
-							puts("Game data improperly formatted; root node must be of type 'NodeQuestion'")
-				end
-			when 5 then
-				exit
-			else
-				puts 'Enter a valid option.'
-		end
-	end
+  begin
+    puts "\nWelcome to Stephen's guessing game!\n"
+    puts '1 - Play the game'
+    puts '2 - View current game tree'
+    puts '3 - Save game to text file'
+    puts '4 - Load game from text file'
+    puts "5 - Quit\n"
+    opt = gets.chomp.to_i
+    # read in user input
+    # ran into the issues of determining how deep a node was in the tree, for printing structured output correctly,
+    # so for now I thought it best to solve the problem using a somewhat 'hacky' but effective approach; globals!
+    # gross! haha. But it works and makes sense. The game is only processing one tree at a time, so it just maintains
+    # the current 'total' tree height and the traversal method calculates the node depth based on the difference.
+    # Obviously this breaks the flavor of our recursive program, but makes sense in as an exception since the information
+    # is not representable per tree-instance. I thought about using a class variable but what's the point? =)
+    $total_height = tree.height
+    case opt
+      when 1 then
+        puts "Think of something and I'll try to guess it."
+        puts "When you're ready, enter 'Y' or 'N'"
+        tree = tree.play if gets.chomp =~ /Y|y/
+        # this it the only part of my program that feels 'uncomfortable'. It breaks with the elegance the rest of the
+        # control flow exhibits. We have to implement this out of recursion however, if we are going to modify the tree
+        # at the root level. It's a bit 'hacky', but does the job.
+        if $new_node
+          tree = $new_node[:child] =~ /Y|y/ ?
+              NodeQuestion.new($new_node[:question], NodeAnswer.new($new_node[:thing]), tree) :
+              NodeQuestion.new($new_node[:question], tree, NodeQuestion.new($new_node[:thing]))
+        end
+      when 2 then
+        puts ''
+        tree.traverse_tree { |val| print val }
+        puts ''
+      when 3 then
+        puts 'Enter a filename, including extension:'
+        fname = gets.chomp
+        if File.exists? fname
+          puts 'File already exists - overwrite?'
+          file = (gets.chomp =~ /Y|y/) ?
+              File.open(fname, 'w+') :
+              File.open(fname_incr(fname), 'w+')
+        else
+          file = File.open(fname, 'w+')
+        end
+        tree.traverse_tree { |val| file.print !$print_with_newline ? val : val + "\n" }
+        file.close
+        puts "Game data saved to '#{file.path}'"
+      when 4 then
+        puts 'Enter a filename, with or without extension:'
+        fname = 'game.txt'
+        files = Dir.glob(fname)
+        if files.empty?
+          puts 'File does not exist.'
+        else
+          file = File.open(files.first)
+          file.gets.strip[0..2] == 'Q: ' ?
+              tree = parse_tree(file.readlines) :
+              puts("Game data improperly formatted; root node must be of type 'NodeQuestion'")
+        end
+      when 5 then
+        exit
+      else
+        puts 'Enter a valid option.'
+    end
+  end
 end
